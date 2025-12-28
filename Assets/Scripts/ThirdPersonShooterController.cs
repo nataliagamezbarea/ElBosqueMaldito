ca845dec81c5d3f9a0786b9fa7b5bb35f7b96afe
nataliagamezbarea <nataliagbarea@gmail.com>
2025-12-28 03:23:45 +0100

    Particula verde señala objetivo

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -3,24 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using StarterAssets;
 
-
 public class ThirdPersonShooterController : MonoBehaviour
 {
     [SerializeField] private GameObject aimVirtualCamera;
     [SerializeField] private float normalSensitivity = 2f;
     [SerializeField] private float aimSensitivity = 0.5f;
-
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField] private Transform debugTransform;
 
     private ThirdPersonController thirdPersonController;
     private StarterAssetsInputs starterAssetsInputs;
-
 
     private void Awake()
     {
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
         thirdPersonController = GetComponent<ThirdPersonController>();
     }
-
 
     private void Update()
     {
@@ -34,8 +32,17 @@ public class ThirdPersonShooterController : MonoBehaviour
             aimVirtualCamera.SetActive(false);
             thirdPersonController.SetSensitivity(normalSensitivity);
         }
+
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransform.position = raycastHit.point;
+        }
+        else 
+        {
+            debugTransform.position = ray.GetPoint(50f);
+        }
     }
 }
-
-
-
